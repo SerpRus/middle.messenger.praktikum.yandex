2400a484d168e314e5167221c13bfca31ea244ee
SerpRus <Slawka00@yandex.ru>
2023-07-30 19:24:13 +0300

    sprint_1 / Добавление favicon, шрифтов, стилей в проект. Удаление лишних файлов

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,22 @@ import handlebars from './vite-plugin-handlebars-precompile';
 
 export default defineConfig({
     root: resolve(__dirname, 'src'),
+    publicDir: resolve(__dirname, 'static'),
     build: {
         outDir: resolve(__dirname, 'dist'),
     },
     plugins: [handlebars()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+                    @import "./src/scss/_vars.scss";
+                    @import "./src/scss/base/_fonts.scss";
+                    @import "./src/scss/base/_generic.scss";
+                    @import "./src/scss/utils/_functions.scss";
+                    @import "./src/scss/utils/_mixins.scss";
+                `,
+            },
+        },
+    },
 });
